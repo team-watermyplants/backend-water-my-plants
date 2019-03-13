2fa8e6d52d87bd4bd9e4df4794d113079652450b
christiansallen <christiansallen@gmail.com>
2019-03-13 11:35:01 -0600

    final repair of env files

--- twilio/Send_sms.js
+++ twilio/Send_sms.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-console.log("ACCOUNT SID-----", accountSid, authToken);
 const client = require("twilio")(accountSid, authToken);
 
 module.exports = {
