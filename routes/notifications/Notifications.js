b66122d17baabfa57a3c49d323d53345e944bce7
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-14 16:59:53 -0700

    fixes serve to handle bulkNotifications

--- routes/notifications/Notifications.js
+++ routes/notifications/Notifications.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const db = require("../../data/dbConfig");
+const db = require('../../data/dbConfig');
 
-router.get("/", async (req, res) => {
-  let notifications = await db("notifications");
+router.get('/', async (req, res) => {
+  let notifications = await db('notifications');
   res.status(200).json(notifications);
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
-  db("notifications")
+  db('notifications')
     .where({ userId: id })
     .then(notification => {
       if (notification.trim().length === 0) {
         res.status(400).json({
-          message: "There are no notifications associated with that id."
+          message: 'There are no notifications associated with that id.',
         });
       } else {
         res.status(200).json(notification);
@@ -24,65 +24,59 @@ router.get("/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { notificationTime, smsDelivered, plantId, userId } = req.body;
   if (!plantId || !userId) {
     res
       .status(400)
-      .json({ message: "Please give the notification a plantId and userId." });
+      .json({ message: 'Please give the notification a plantId and userId.' });
   } else {
-    db("notifications")
+    db('notifications')
       .insert({ notificationTime, smsDelivered, plantId, userId })
-      .returning("*")
+      .returning('*')
       .then(notification => res.status(201).json(notification))
       .catch(err => res.status(500).json(err));
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   const { id } = req.params;
   const { notificationTime, smsDelivered, plantId, userId } = req.body;
 
-  if (!plantId || !userId) {
-    res
-      .status(400)
-      .json({ message: "Please give the notification a plantId and userId." });
-  } else {
-    db("notifications")
-      .where({ id: id })
-      .update({ notificationTime, smsDelivered, plantId, userId })
-      .returning("*")
-      .then(notification => {
-        if (notification) {
-          res.status(200).json(notification);
-        } else if (!notification) {
-          res
-            .status(404)
-            .json({ message: "There isn't anything to update with that id." });
-        }
-      })
-      .catch(() => res.status(500).json({ message: "server error" }));
-  }
+  db('notifications')
+    .where({ id: id })
+    .update({ notificationTime, smsDelivered, plantId, userId })
+    .returning('*')
+    .then(notification => {
+      if (notification) {
+        res.status(200).json(notification);
+      } else if (!notification) {
+        res
+          .status(404)
+          .json({ message: "There isn't anything to update with that id." });
+      }
+    })
+    .catch(() => res.status(500).json({ message: 'server error' }));
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  db("notifications")
+  db('notifications')
     .where({ id: id })
     .del()
     .then(notification => {
       if (!notification) {
         res.status(400).json({
           message:
-            "There are no notifications to delete corresponding with that id."
+            'There are no notifications to delete corresponding with that id.',
         });
       } else {
         res
           .status(200)
-          .json({ message: "The notification was successfully deleted." });
+          .json({ message: 'The notification was successfully deleted.' });
       }
     })
-    .catch(() => res.status(500).json({ message: "server error" }));
+    .catch(() => res.status(500).json({ message: 'server error' }));
 });
 
 module.exports = router;
